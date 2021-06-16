124a40abdcf901747113f7f63d701f322aaaedd2
YamGoel <85970552+YamGoel@users.noreply.github.com>
2021-06-16 05:59:49 +0530

    Update my_extras.py

--- template_learning/basic_app/templatetags/my_extras.py
+++ template_learning/basic_app/templatetags/my_extras.py
@@ -4,7 +4,9 @@ register = template.Library()
 
 @register.filter(name='cut')
 def cut(value,arg):
-    
+    """
+    this cuts out all values of arg from string
+    """
     return value.replace(arg,'')
 
 # register.filter('cut',cut)
